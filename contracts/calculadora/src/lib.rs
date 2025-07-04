2ea6cd24f93feb224eacda3d544219b83cd95269
laulefanta <laulefanta@gmail.com>
2025-07-04 18:18:53 -0500

    Implemente el  hacer la suma Y luego almacené el resultado:

--- contracts/calculadora/src/lib.rs
+++ contracts/calculadora/src/lib.rs
@@ -1,23 +1,25 @@
 #![no_std]
 
+use soroban_sdk::{contract, contractimpl, Env, Symbol, symbol_short};
 
-use soroban_sdk::{contract, contractimpl,  Env, symbol_short, Symbol};
 const RESULTADO: Symbol = symbol_short!("RESULTADO");
+
 #[contract]
 pub struct Contract;
 
-
 #[contractimpl]
 impl Contract {
-    
-    pub fn sumar(env: Env, a:i128, b:i128) -> i128 {
-      //Implementar función que sume dos números
-      return 30;
+    pub fn sumar(env: Env, a: i128, b: i128) -> i128 {
+        let resultado = a + b;
+        env.storage().persistent().set(&RESULTADO, &resultado);
+        resultado
     }
 
     pub fn resultado_anterior(env: Env) -> i128 {
-           //Implementar función que retorne el valor anterior
-            return 30;
+        match env.storage().persistent().get::<Symbol, i128>(&RESULTADO) {
+            Some(r) => r,
+            None => 0,
+        }
     }
 }
 
